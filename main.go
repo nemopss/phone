04e5a4dbd9bd49cb2e7da118cf93bf8e2b8ee663
nemopss <alexey_gladilin@mail.ru>
2024-06-21 17:55:20 +0300

    Create DB

--- main.go
+++ main.go
@@ -1,13 +1,62 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"regexp"
+
+	_ "github.com/lib/pq"
 )
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "phone"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+	err = resetDB(db, dbname)
+	must(err)
+	db.Close()
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+
+	must(db.Ping())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func normalize(phone string) string {
 	re := regexp.MustCompile("[^0-9]")
-
 	return re.ReplaceAllString(phone, "")
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+	return createDB(db, name)
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // func normalize(phone string) string {
