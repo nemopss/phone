811d23076c43037df44d0e9361e3b2beb21a6160
nemopss <alexey_gladilin@mail.ru>
2024-07-02 05:10:43 +0300

    Add phones querying

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 	must(err)
 	_, err = insertPhone(db, "123 456 7891")
 	must(err)
-	_, err = insertPhone(db, "(123) 456 7892")
+	id, err := insertPhone(db, "(123) 456 7892")
 	must(err)
 	_, err = insertPhone(db, "(123) 456-7893")
 	must(err)
@@ -46,6 +46,51 @@ func main() {
 	must(err)
 	_, err = insertPhone(db, "(123)456-7892")
 	must(err)
+
+	number, err := getPhone(db, id)
+	must(err)
+	fmt.Println("Number =", number)
+
+	phones, err := allPhones(db)
+	must(err)
+	for _, p := range phones {
+		fmt.Printf("%+v\n", p)
+	}
+}
+
+func getPhone(db *sql.DB, id int) (string, error) {
+	var number string
+	err := db.QueryRow("SELECT id, value FROM phone_numbers WHERE id=$1", id).Scan(&id, &number)
+	if err != nil {
+		return "", err
+	}
+	return number, nil
+}
+
+type phone struct {
+	id     int
+	number string
+}
+
+func allPhones(db *sql.DB) ([]phone, error) {
+	rows, err := db.Query("SELECT id, value FROM phone_numbers")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var ret []phone
+	for rows.Next() {
+		var p phone
+		if err := rows.Scan(&p.id, &p.number); err != nil {
+			return nil, err
+		}
+		ret = append(ret, p)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return ret, nil
 }
 
 func insertPhone(db *sql.DB, phone string) (int, error) {
