3b42d9297d1306396819b6c3e51fc4e67cef96b7
nemopss <alexey_gladilin@mail.ru>
2024-07-02 06:00:02 +0300

    Add phone number normalizing

--- main.go
+++ main.go
@@ -54,7 +54,21 @@ func main() {
 	phones, err := allPhones(db)
 	must(err)
 	for _, p := range phones {
-		fmt.Printf("%+v\n", p)
+		fmt.Printf("Working on... %+v\n", p)
+		number := normalize(p.number)
+		if number != p.number {
+			fmt.Println("Updating or removing...", number)
+			existing, err := findPhone(db, number)
+			must(err)
+			if existing != nil {
+				must(deletePhone(db, p.id))
+			} else {
+				p.number = number
+				must(updatePhone(db, p))
+			}
+		} else {
+			fmt.Println("No changes required")
+		}
 	}
 }
 
@@ -65,6 +79,31 @@ func getPhone(db *sql.DB, id int) (string, error) {
 		return "", err
 	}
 	return number, nil
+}
+
+func findPhone(db *sql.DB, number string) (*phone, error) {
+	var p phone
+	err := db.QueryRow("SELECT id, value FROM phone_numbers WHERE value=$1", number).Scan(&p.id, &p.number)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		} else {
+			return nil, err
+		}
+	}
+	return &p, nil
+}
+
+func updatePhone(db *sql.DB, p phone) error {
+	statement := `UPDATE phone_numbers SET value=$2 WHERE id=$1`
+	_, err := db.Exec(statement, p.id, p.number)
+	return err
+}
+
+func deletePhone(db *sql.DB, id int) error {
+	statement := `DELETE FROM phone_numbers WHERE id=$1`
+	_, err := db.Exec(statement, id)
+	return err
 }
 
 type phone struct {
